3ee507ff4256f6056de683748e69089088da795e
JeremiasGaleano <63808502+JeremiasGaleano@users.noreply.github.com>
2020-06-30 20:13:56 -0300

    DAOPRODUCTOS
    dao

--- TrabajoFinal/Entidades/Productos.cs
+++ TrabajoFinal/Entidades/Productos.cs
@@ -9,7 +9,7 @@ namespace Entidades
     //escribo todos los campos que va a tener la entidad.
     public class Productos
     {
-        string codigo_articulo; //completado
+        string codigo_producto; //completado
         string nombre_producto; //completado
         decimal precio_producto; //complteado
         bool estado;    //completado
@@ -20,14 +20,14 @@ namespace Entidades
 
         }
 
-        public void set_codigo_articulo(string nuevo_codigo_articulo)
+        public void set_codigo_producto(string nuevo_codigo_producto)
         {
-            this.codigo_articulo = nuevo_codigo_articulo;
+            this.codigo_producto = nuevo_codigo_producto;
         }
 
-        public string get_codigo_articulo()
+        public string get_codigo_producto()
         {
-            return this.codigo_articulo;
+            return this.codigo_producto;
         }
 
         public void set_nombre_producto(string nuevo_nombre_producto)
