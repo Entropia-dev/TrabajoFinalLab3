9b5be772c8d2737996c86f4a4b21192ef2786ba8
JAaron97 <63803899+JAaron97@users.noreply.github.com>
2020-07-01 01:09:28 -0300

    Update revestimientoayt.sql
    BASE DE DATOS RENOVADA

--- TrabajoFinal/revestimientoayt.sql
+++ TrabajoFinal/revestimientoayt.sql
@@ -1,24 +1,11 @@
 
+
 create database Revestimientoayt
 
 GO 
 
 USE  Revestimientoayt
 GO
-CREATE TABLE Cuentas(
-
-Correo VARCHAR (20) NOT NULL,
-Pass_Cue CHAR(8)NOT NULL,
-Num_Doc_Cue CHAR(8)NOT NULL,
-Usuario_Cue CHAR(8)NOT NULL,
-CP_Cue CHAR(8)NOT NULL,
-
-
-CONSTRAINT PK_Cuentas PRIMARY KEY  (Correo)
-
-
-
-)
 
  CREATE TABLE Clientes (
 Correo_Cli  VARCHAR (20) NOT NULL,
@@ -31,24 +18,39 @@ Dni CHAR(8)NOT NULL,
 Direccion VARCHAR (20) NOT NULL,
 Usuario_Cli CHAR(8)NOT NULL,
 
-CONSTRAINT PK_Clientes PRIMARY KEY  (Correo_Cli)
+CONSTRAINT PK_Clientes PRIMARY KEY  (Dni)
       
   
   )
   
- CREATE TABLE Sesion (
- Usurio_Ses CHAR(8) NOT NULL,
-  Correo_Ses VARCHAR(20),
-   CONSTRAINT PK_Sesion PRIMARY KEY  (Usurio_Ses)
-  ) 
+CREATE TABLE Cuentas(
+
+Correo VARCHAR (20) NOT NULL,
+Pass_Cue CHAR(8)NOT NULL,
+Dni CHAR (8) NOT NULL,
+Alias CHAR(8)NOT NULL,
+CP_Cue CHAR(8)NOT NULL,
+Nivel_cuenta  bit ,--boleano
+
+CONSTRAINT PK_Cuentas PRIMARY KEY  (Correo),
+
+
+CONSTRAINT FK_Cuentas_clientes foreign KEY  (Dni) references  Clientes (dni)
+
+)
+
+
+  
+
   
    CREATE TABLE Ventas (
   Num_Doc_Com char(8) NOT NULL,
 Total  DECIMAL(8,2) DEFAULT '0,00',
- Nom_Usu_Com char(8) NOT NULL,
- Cod_Cli char(8) NOT NULL,
- Id char(8) NOT NULL,
+ Alias char(8) NOT NULL,
+Fecha  smalldatetime  default getdate(),
    CONSTRAINT PK_Ventas PRIMARY KEY  (Num_Doc_Com)
+   CONSTRAINT FK_Ventas_clientes foreign KEY  (Dni) references  Clientes (dni)
+
     )
    
   
@@ -56,10 +58,10 @@ Total  DECIMAL(8,2) DEFAULT '0,00',
   Num_Doc_Det  char(8) not null,
 Cantidad char(8) NOT NULL,
  PU_Det DECIMAL(8,2) DEFAULT '0,00',
- Subtotal DECIMAL(8,2) DEFAULT '0,00',
+ 
   Cod_Art_Det char(8) NOT NULL,
-   CONSTRAINT PK_Detalle PRIMARY KEY  ( Num_Doc_Det)
 
+  CONSTRAINT PK_Detalle PRIMARY KEY  ( Cod_Art_Det,Num_Doc_Det)
     )
     
    CREATE TABLE Productos (
@@ -73,6 +75,28 @@ Cantidad char(8) NOT NULL,
    Cuit_Pro char(8) NOT NULL,
  Razon_Social char(8) NOT NULL,
  Contacto char(8) NOT NULL,
+ Direccion char(8) NOT NULL,
+ cbu char(8) NOT NULL,
     CONSTRAINT PK_Proveedores PRIMARY KEY  (  Cuit_Pro)
     )
     
+    
+       CREATE TABLE Compra  (
+  Cod_compra char(8) NOT NULL,
+Total  DECIMAL(8,2) DEFAULT '0,00',
+ cuit_proveedor char(8) NOT NULL,
+Fecha  smalldatetime  default getdate(),
+   CONSTRAINT PK_Compra PRIMARY KEY  (Cod_compra),
+   CONSTRAINT FK_Compra_Proveedores foreign KEY  (cuit_proveedor) references  Proveedores (Cuit_Pro)
+
+    )
+    
+     create table Detalle_compra (
+  Num_Doc_Det  char(8) not null,
+Cantidad char(8) NOT NULL,
+ PU_Det DECIMAL(8,2) DEFAULT '0,00',
+ 
+  Cod_Art_Det char(8) NOT NULL,
+
+  CONSTRAINT PK_Detalle PRIMARY KEY  ( Cod_Art_Det,Num_Doc_Det)
+    )
